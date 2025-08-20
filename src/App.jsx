7b9afebd0a052132a08897175892e740788d57fa
Kiran Gadamsetty <gadamsettykiran514@gmail.com>
2025-08-20 12:58:24 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Header from "./components/Header.jsx"
 import "./App.css"
 import Sidebar from "./components/Sidebar.jsx"
 import QrPage from "./Pages/QrPage.jsx"
-import {createBrowserRouter, Outlet} from "react-router-dom"
+import { createBrowserRouter, Outlet } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import HistoryPage from "./pages/HistoryPage.jsx"
 import Profile from "./pages/Profile.jsx"
 import ErrorElement from "./utils/ErrorElement.jsx"
+
 function App() {
   return (
     <div>
@@ -16,46 +17,47 @@ function App() {
           display: "flex",
           justifyContent: "center",
           width: "100%",
-         
         }}
       >
         <div
-  className="main-layout"
-  style={{
-    display: "flex",
-    gap: "30px",
-    maxWidth: "1400px",
-    width: "100%",
-    padding: "0 20px",   // ✅ default left & right spacing
-    boxSizing: "border-box",
-  }}
->
-  <Sidebar />
-  <div style={{ flexGrow: 1 }}>
-    <Outlet />
-  </div>
-</div>
-
+          className="main-layout"
+          style={{
+            display: "flex",
+            gap: "30px",
+            maxWidth: "1400px",
+            width: "100%",
+            padding: "0 20px",
+            boxSizing: "border-box",
+          }}
+        >
+          <Sidebar />
+          <div 
+            style={{ 
+              flexGrow: 1,
+              display: "flex",
+              justifyContent: "center", // This centers the content
+              alignItems: "flex-start", // Aligns content to the top
+              width: "100%" // Ensures this container takes up remaining space
+            }}
+          >
+            <Outlet />
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-
 export const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-     errorElement: <ErrorElement />,
+    errorElement: <ErrorElement />,
     children: [
-      { path: "/", element: <Home />,errorElement: <ErrorElement /> },
-      { path: "/qrpage", element: <QrPage /> ,errorElement: <ErrorElement />},
-      { path: "/profile", element: <Profile /> ,errorElement: <ErrorElement />},
-      { path: "/history", element: <HistoryPage />,errorElement: <ErrorElement /> },
+      { path: "/", element: <Home />, errorElement: <ErrorElement /> },
+      { path: "/qrpage", element: <QrPage />, errorElement: <ErrorElement /> },
+      { path: "/profile", element: <Profile />, errorElement: <ErrorElement /> },
+      { path: "/history", element: <HistoryPage />, errorElement: <ErrorElement /> },
     ],
-   
   },
-]);
-
-
-
+])
